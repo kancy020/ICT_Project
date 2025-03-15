82dce66538d183618830c204953f54a68e6f7507
kancy020 <kancy020@mymail.unisa.edu.au>
2025-03-15 11:35:18 +1030

    setting up flask url request to the slack api

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 from flask import Flask
+
+from flask import Flask, Response
+from flask import request as flask_request
+
+#setting up flask file
 app = Flask(__name__)
+
+#get method to produce a response on the url 
+@app.route("/", methods=['GET'])
+def hello():
+    return Response("Hello, World!"), 200
+
+#post method to test the generated token
+@app.route('/verify', methods=['POST'])
+def inbound():
+    """
+    testing to see if token worked
+    """
+    #payload from flask handles the JSON file request
+    payload = flask_request.get_json()
+
+    #once the challenge request it recieved repond to successfully initialise request
+    if payload:
+        return Response(payload['challenge']), 200
