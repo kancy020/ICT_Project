99a49c013bc26f3bb50a735aa48c8716a0327721
kancy020 <kancy020@mymail.unisa.edu.au>
2025-03-19 10:35:39 +1030

    restarting from stractch

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, Response
-from flask import request as flask_request
 
-#setting up flask file
-app = Flask(__name__)
-
-#get method to produce a response on the url 
-@app.route('/slack/command', methods=['POST'])
-def command():
-    data = flask_request.form
-    return Response("command is recieved", status=200)
-
-if __name__ == '__main__':
-    app.run(debug=True)
