3d9f5d2a2932c27f0d1841661dcb994cd41478cd
taoye019 <166194667+taoye019@users.noreply.github.com>
2025-03-20 14:21:06 +1030

    order map

---
+++ reading.py
@@ -0,0 +1,18 @@
+# Slack 
+EMOJI_MAP = {}
+
+# # Default command mapping table
+COMMANDS = {
+    "show": ,
+    "turn off": ,
+    "turn on": ,
+    "flip": ,
+    "sync time": 
+}
+
+# test
+sample_messages = [
+    ":grinning: turn off display",
+]
+
+# test print
