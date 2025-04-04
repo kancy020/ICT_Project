69b70a2c478841722cdce1bc33c101048282a789
ZHUOLIANG XIE <115598284+StevenXZL@users.noreply.github.com>
2025-04-04 14:47:24 +1030

    set Raspberry Pi IP in send.py

--- send.py
+++ send.py
@@ -1,7 +1,8 @@
-
 import requests
 
 #raspPi = put in raspberry pi IP <-- delete up to equals sign, remove the comment and place the raspPi ip after the equals symbol #  #here 
+
+raspPi = "http://192.168.68.110:5000"
 
 def send_emoji(emoji):
     payload = {"emoji": emoji}
@@ -10,7 +11,8 @@ def send_emoji(emoji):
 
 def default_welcome_sign():
     payload = {"text": "Welcome"}
-    response = requests.post(rasPi, json=payload)
+    response = requests.post(raspPi, json=payload)
     return response.json()
+
 
 
