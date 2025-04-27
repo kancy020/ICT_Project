14abcb6da03a37762e074e577f3c337377226caf
ZHUOLIANG XIE <XIEZY012@mymail.unisa.edu.au>
2025-04-27 22:31:19 +0930

    Set up Flask server for receiving and testing Pixel Display communication

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
-import send
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    print("Received data:", data)
+    return jsonify({"status": "success", "data": data}), 200
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    print("Flask server starting...")
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
